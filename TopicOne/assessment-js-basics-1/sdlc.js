79bcd853e9157d5324ccd2f1fc2d2a57af4626ec
drewriker <62220518+drewriker@users.noreply.github.com>
2024-01-22 21:03:53 -0700

    Update sdlc.js

--- TopicOne/assessment-js-basics-1/sdlc.js
+++ TopicOne/assessment-js-basics-1/sdlc.js
@@ -20,8 +20,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
-const analysisOfRequirements = ""
-const design = ""
-const implementation = ""
-const testing = ""
-const maintain = ""
+const analysisOfRequirements = "Work with the stakeholders to understand the requirement for the software"
+const design = "Creating the architecture and basic design of the software with the given requirements"
+const implementation = "Start the code of the software based off the design specifications"
+const testing = "Running tests to find any bugs in the code. Then deploying the code to make available to customers"
+const maintain = "Keep fixing bugs, and pushing updates to increase user experience and security"
